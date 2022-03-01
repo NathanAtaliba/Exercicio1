6e07cf420cbb68aaeffa3f5520459b98f2247a2f
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-03-01 11:11:39 -0300

    Produto

--- Exercicios de classes/src/main/java/Produto.java
+++ Exercicios de classes/src/main/java/Produto.java
@@ -2,7 +2,7 @@ public class Produto {
     void mostraInfo() {
         System.out.println("Nome do produto: " + Nome);
         System.out.println("Serie do produto: " + codigoSerie);
-        System.out.println("Categoria do produto: " + categoria);
+        System.out.println("Categioria do produto: " + categoria);
         System.out.println("Codigo do produto: " + codigoProduto);
     }
         int codigoSerie;
