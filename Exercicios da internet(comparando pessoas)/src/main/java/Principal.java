4ef0f4cc601d49dd1b0d48d0c985c2727f63f9b5
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-03-08 00:05:01 -0300

    Exercicios da internet(Conta pessoas)

---
+++ Exercicios da internet(comparando pessoas)/src/main/java/Principal.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Principal {
+    public static void main(String[] args) {
+        int sexo=0, qtMulheres = 0, qtHomens = 0;
+        float altura=0, somaH = 0, mediaHomens = 0, maior = 0, menor = 0;
+
+        Scanner entrada = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Escolha o sexo da pessoa (1-Para mulher, 2-Para homem): ");
+            sexo = entrada.nextInt();
+            System.out.print("Digite a altura da pessoa: ");
+            altura = entrada.nextFloat();
+            if (sexo == 1) {
+                qtMulheres++;
+            } else if (sexo == 2) {
+                qtHomens++;
+                somaH = somaH + altura;
+            } else {
+                System.out.println("Opção sexo inválido!");
+            }
+            if (altura > maior) {
+                maior = altura;
+            } else if (altura < menor) {
+                menor = altura;
+            }
+        }
+        mediaHomens = somaH / qtHomens;
+
+        System.out.println("A maior altura do grupo é de " + maior + " m, e a menor é de " + menor + " m");
+        System.out.println("A média de altura dos homens é " + mediaHomens + " m");
+        System.out.println("O número de mulheres é " + qtMulheres);
+
+        entrada.close();
+    }
+}
