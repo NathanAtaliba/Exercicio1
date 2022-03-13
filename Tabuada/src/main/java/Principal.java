c5387490428d2c961c3c7075c80eb3f1b9602bda
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-03-13 14:33:21 -0300

    tabuada

---
+++ Tabuada/src/main/java/Principal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Principal {
+  static void tabuada(int N){
+        System.out.println("tabuada: ");
+        System.out.println("1 x "+(N)+" = "+ (N*1));
+        System.out.println("2 x "+(N)+" = "+ (N*2));
+        System.out.println("3 x "+(N)+" = "+ (N*3));
+        System.out.println("4 x "+(N)+" = "+ (N*4));
+        System.out.println("5 x "+(N)+" = "+ (N*5));
+        System.out.println("6 x "+(N)+" = "+ (N*6));
+        System.out.println("7 x "+(N)+" = "+ (N*7));
+        System.out.println("8 x "+(N)+" = "+ (N*8));
+        System.out.println("9 x "+(N)+" = "+ (N*9));
+        System.out.println("10 x "+(N)+" = "+ (N*10));
+
+
+    }
+    public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+        int n;
+        System.out.println("Entre com o numero que voce quer a tabuada: ");
+        n = input.nextInt();
+        tabuada(n);
+    }
+}
