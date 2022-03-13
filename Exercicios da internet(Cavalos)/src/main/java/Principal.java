bda51c2886ebc4e4ca08a010449052c7bb7bcc95
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-03-13 14:32:37 -0300

    Exercicios da internet( Cavalos)

---
+++ Exercicios da internet(Cavalos)/src/main/java/Principal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Principal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double cavalos;
+        double massa=0;
+        double altura=0;
+        double tempo=0;
+        System.out.println("Entre com o valor da massa a ser levantada: ");
+         massa = input.nextDouble();
+         input.nextLine();
+        System.out.println("Entre com o valor do tempo: ");
+        tempo = input.nextDouble();
+        input.nextLine();
+        System.out.println("Entre com a altura: ");
+        altura = input.nextDouble();
+        input.nextLine();
+        cavalos =((massa*altura)/tempo)/745.699;
+        System.out.println("Vai precisar de :"+ cavalos +" cavalos");
+
+    }
+
+}
