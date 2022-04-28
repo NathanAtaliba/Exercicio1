3d4269163db3f4b02dee9aa3179d6ae0cb1e6388
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-04-28 13:21:41 -0300

    Colecoes

---
+++ Colecoes/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        ArrayList<Produto> produtos = new ArrayList<>();
+        Produto p1 = new Produto("Manteiga" ,3);
+        Produto p2 = new Produto("Margarina",4);
+        Produto p3 = new Produto("Caderno",10);
+        Produto p4 = new Produto("Lapis",1);
+        Produto p5 = new Produto("Borracha",2);
+        produtos.add(p1);
+        produtos.add(p2);
+        produtos.add(p3);
+        produtos.add(p4);
+        produtos.add(p5);
+
+        Collections.reverseOrder();
+        for(Produto produto : produtos){
+            System.out.println(produto.getNome());
+            System.out.println(produto.getPreco());
+            System.out.println("");
+        }
+
+
+
+    }
+    }
+
---
+++ Colecoes/src/main/java/Produto.java
@@ -0,0 +1,36 @@
+public class Produto implements Comparable<Produto> {
+    private String nome;
+private int preco;
+public Produto(String nome, int preco){
+    this.preco = preco;
+    this.nome = nome;
+
+}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void mostraInfo(){
+    System.out.println(nome);
+    System.out.println(preco);
+
+}
+@Override
+    public int compareTo(Produto p){
+return nome.compareTo(p.nome);
+
+}
+}
