6b3aeaaaf023403ea255750520a5495e266eec57
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-05-20 23:03:05 -0300

    Try_Catch3

---
+++ Try_Catch3/src/main/java/Conta.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+public class Conta {
+    private int saldo;
+    private int limite;
+
+    HashSet<Cliente> clientes = new HashSet<>();
+    public Conta(int saldo,int limite){
+        this.saldo = saldo;
+        this.limite = limite;
+    }
+    public void mostraInfo(){
+        for(Cliente cliente: clientes){
+            try {
+                System.out.println(cliente.getNome());
+            }catch(NullPointerException e){
+                System.out.println("Erro: "+ e);
+            }
+            }
+        }
+public void sacar(int valor){
+        if(saldo>=valor) {
+            saldo = saldo - valor;
+        }else{
+            throw new SaldoInsuficienteException("Saldo Insuficiente") ;
+        }}
+    public void addCliente(Cliente cliente){
+        clientes.add(cliente);
+    }
+}
+
---
+++ Try_Catch3/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+public class Main {
+    public static void main(String[] args) {
+        Conta conta = new Conta(1000,100);
+        Cliente cliente1 = new Cliente("Nathan",1);
+        Cliente cliente2 = new Cliente("Renan",2);
+        Cliente cliente3 = null;
+        conta.addCliente(cliente1);
+        conta.addCliente(cliente2);
+        conta.addCliente(cliente3);
+        conta.mostraInfo();
+conta.sacar(1000);
+    }
+}
